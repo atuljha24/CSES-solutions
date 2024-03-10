7eabf56fe2d67c5b24d4dc6fb0ca27578b7cac64
Atul Jha <2140207@sliet.ac.in>
2024-03-10 23:58:09 +0530

    hogya commit

---
+++ bitstring.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+   long long n;
+   cin>>n;
+   
+   cout<<pow(2,n);}
+    
